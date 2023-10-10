43749c557bf0fba6495151ee466fed0c8785f576
vinhyenvodoi98 <33363513+vinhyenvodoi98@users.noreply.github.com>
2023-10-10 15:02:16 +0900

    Update Header.tsx

--- packages/client/src/components/layout/Header.tsx
+++ packages/client/src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import Wallet from '@/components/Providers/wallet';
 
 export default function Header() {
   return (
-    <div className='z-100 p-4 sticky top-0 bg-transparent'>
+    <div className='z-10 p-4 sticky top-0 bg-transparent'>
       <div className='navbar shadow-xl rounded-box border'>
         <div className='flex-1'>
           <Link href='/' className='btn btn-ghost normal-case text-xl'>
