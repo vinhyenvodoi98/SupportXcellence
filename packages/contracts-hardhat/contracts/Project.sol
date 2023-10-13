d7eb85d5c48ca8c7bb8b8c89fd3c8c00721265d4
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-10-13 21:30:07 +0900

    chore: forge init

--- packages/contracts-hardhat/contracts/Project.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract Project is Ownable {
-  constructor(address _owner)
-    Ownable(_owner)
-  {}
-}
